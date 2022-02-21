c534dfe9f7780491989ea15e89fbf812a3f53cde
Jorge Gomez <57576609+JorgeGomezAZ@users.noreply.github.com>
2022-02-21 16:44:36 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from 'react'
-import Portfolio from './Portfolio'
 import { Route, Routes } from "react-router-dom";
 import Palabraz from './Palabraz';
-import Tetris from './components/tetris/Tetris';
 
 function App()
 {
   return (
     <div className='App'>
       <Routes>
-        <Route path='/' element={<Portfolio />} />
-        <Route path='/portfolio' element={<Portfolio />} />
+        <Route path='/' element={<Palabraz />} />
         <Route path='/palabraz' element={<Palabraz />} />
-        <Route path='/tetris' element={<Tetris />} />
-        {/* <Route path="*" element={<Portfolio />} /> */}
       </Routes>
     </div>
   )
